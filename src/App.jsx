991875e1ca63e9d7ca5ce3c8df81178b96f11438
your-ehsan <ehsanshahid787@gmail.com>
2023-07-17 20:22:44 +0500

    initial commit
    Initial commit for  the version 1 of Markdown NoteApp and also tailwindcss initialized with this.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+    <h1 className="underline">Tis is tailwindcss</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,css,js,ts,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
